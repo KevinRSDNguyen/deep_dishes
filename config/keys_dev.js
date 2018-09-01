991793e45af469032d1f1f13d615c8b2b112edbb
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-08-31 21:46:05 -0700

    Fixed gitignoreagain

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://kevin:JbZ864ZbJ@ds235431.mlab.com:35431/deepdishes",
-  SECRET: "secret",
-  EMAIL_PASS: "JbZ864ZbJ",
-  ROOT_URL: "http://localhost:3000"
-};
